6b9d9f6b40d0fea73556649a8ab2089fda5ad86b
Shahadat Shuvo <shuvombm02@gmail.com>
2019-03-07 19:38:46 +0600

    Update gallery-shortcode.php

--- vc-image-gallery/gallery-shortcode.php
+++ vc-image-gallery/gallery-shortcode.php
@@ -15,7 +15,7 @@ function themename_gallery_shortcode($atts = array(), $content = null)
         <div class="wagtail-gallery">';
             foreach ($image_ids as $img) {
                 $attachment = wp_get_attachment_image_src( $img, 'full' ); 
-                $markup .= '<img src="'.esc_url($attachment[0]).'"></a>';
+                $markup .= '<img src="'.esc_url($attachment[0]).'">';
             }
     $markup .= '
         </div>';
